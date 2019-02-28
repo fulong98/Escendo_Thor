034ddbcdcff75a1d2e5defe19261dfca3f7f9bfb
fulong98 <fulong98@hotmail.com>
2019-02-28 22:56:48 +0800

    level class

---
+++ thor/Level/testing/testing.ino
@@ -0,0 +1,21 @@
+#include <Level.h>
+
+Level game(1) ;
+void setup() {
+	
+  Serial.begin(9600);
+    
+}
+
+void loop(){
+  int score;
+  score=game.getScore();
+    game.setScore();
+    Serial.println(score);
+    
+    score=game.getScore();
+    Serial.println(score);
+    game.setColor(0);
+    if (score<-100){game.setColor(1);}
+    
+}
